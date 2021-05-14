833454cfa047edc74e2e1ecc2bcf53ecfb1d9bd6
HyunwookYoo <hyunwook5636@gmail.com>
2021-05-14 23:28:57 +0900

    Make host button to do host the game

---
+++ Source/PuzzlePlatforms/MenuSystem/MainMenu.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MenuInterface.h"
+#include "MainMenu.generated.h"
+
+class UButton;
+
+UCLASS()
+class PUZZLEPLATFORMS_API UMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual bool Initialize() override;
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	UButton* Host;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* Join;
+
+	UFUNCTION()
+	void HostServer();
+
+	IMenuInterface* MenuInterface = nullptr;
+};
